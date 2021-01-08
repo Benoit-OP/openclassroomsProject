ebf48bd213ffb92970fea95cdc1d895003a079e8
Benoit Perussel <benoit.14120@hotmail.fr>
2021-01-08 22:28:00 +0100

    ajout du partenaire 2

---
+++ Page_CDE.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="uft-8"/>
+        <title>GBAF, Groupement banques et assurances</title>
+        <link rel="stylesheet" href="Style2.css">
+    </head>
+
+    <body>
+        <div id="bloc_page">
+    
+            <header class="site-header">
+
+                <img src="C:/Users/Benoit/Openclassrooms/openclassroomsProject/Logo_GBAF.jpg" class= "logo_petit" alt= "Logo"/>
+
+             </header>
+                        
+             <img src="C:/Users/Benoit/Openclassrooms/openclassroomsProject/bannière.jpg" class="banniere" alt="Bannière du site"/>
+
+             <section id="acteurs">
+                
+    
+        <br/>  
+                <div id="cadre">
+                    <p><img src="C:Users/Benoit/Openclassrooms/openclassroomsProject/CDE.png" alt="Logo CDE"/></p>
+        <br/>             
+                <p> La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation. 
+                    Son président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.
+                    
+                </p>
+
+
+
+
+                </div>
+
+             </section>
+        
+             <section id="Commentaires">
+                
+                <div class="Mon_com">
+                    <h3>Mon commentaire</h3>
+                    
+                    <textarea name="commentary" rows="4"  placeholder="Commentez..."></textarea>
+                
+                
+                
+                </div>
+                            
+                
+                    
+                       
+            
+            
+            
+            
+            
+            
+            </section> 
+        
+        
+        
+            <footer class="site-footer">
+                <p class="mentions"> Mentions légales | Contacts</p> 
+                 
+     
+     
+             </footer>
+        
+        
+        </div>
+    </body>
+</html>
