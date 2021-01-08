c72662d1f58031e5e2be61aaaba7feef130fc7df
Benoit Perussel <benoit.14120@hotmail.fr>
2021-01-08 03:36:30 +0100

    ajout des fichiers PHP avant BDD

---
+++ GBAF/GBAFconnexion.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="uft-8"/>
+        <title>GBAF, Groupement banques et assurances</title>
+        <link rel="stylesheet" href="Style2.css">
+    </head>
+
+    <body>
+        <div id="bloc_page">
+    
+            <header class="site-header">
+
+                <img src="C:/MAMP/htdocs/test/GBAF/ImagesLogo_GBAF.jpg" class= "logo_petit" alt= "Logo"/>
+
+             </header>
+                        
+             <img src="C:/MAMP/htdocs/test/GBAF/Imagesbannière.jpg" class="banniere" alt="Bannière du site"/>
+
+            
+
+        <br/>
+        <br/>
+                    <h1>Connexion</h1>
+          
+                    <p>Veuillez entrer le mot de passe pour obtenir les codes d'accès au serveur central de la NASA :</p>
+        <form action="accueil-GBAF.php" method="post">
+            <p>
+            <input type="password" name="mot_de_passe" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
+
+
+
+
+
+        <br/>
+            <p><strong> Vous devez vous connecter pour vous accéder au site du GBAF.</strong></p>
+            <br/>
+        <br/>
+        
+            <p>Pas encore membre? <a href="GBAF_inscription.php"> Inscrivez-vous</a> ici.</p>
+            <p>Mot de passe oublié? <a href=""> Cliquez ici</a>.</p>
+
+
+
+
+            <footer class="site-footer">
+           <p class="mentions"> Mentions légales | Contacts</p> 
+            
+
+
+        </footer>
+
+
+
+
+        </div>
+    </body>
+</html>
