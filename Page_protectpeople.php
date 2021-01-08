8234e7ff52bd5a2460897c10f0e53f675de7440a
Benoit Perussel <benoit.14120@hotmail.fr>
2021-01-08 22:29:38 +0100

    ajout du partenaire 3

---
+++ Page_protectpeople.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="uft-8"/>
+        <title>GBAF, Groupement banques et assurances</title>
+        <link rel="stylesheet" href="Style2.css">
+    </head>
+
+    <body>
+        <div id="bloc_page">
+    
+            <header class="site-header">
+
+                <img src="C:/Users/Benoit/Openclassrooms/openclassroomsProject/Logo_GBAF.jpg" class= "logo_petit" alt= "Logo"/>
+
+             </header>
+                        
+             <img src="C:/Users/Benoit/Openclassrooms/openclassroomsProject/bannière.jpg" class="banniere" alt="Bannière du site"/>
+
+            <section id="acteurs">
+        
+        <br/>
+            <div id="cadre">
+                <p> <img src="C:Users/Benoit/Openclassrooms/openclassroomsProject/protectpeople.png" alt="Logo ProtectPeople"/> </p>
+        <br/>
+
+                <p> Protectpeople finance la solidarité nationale.
+                    Nous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.
+                    
+                    Chez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins.
+                    Proectecpeople est ouvert à tous, sans considération d’âge ou d’état de santé.
+                    Nous garantissons un accès aux soins et une retraite.
+                    Chaque année, nous collectons et répartissons 300 milliards d’euros.
+                    Notre mission est double :
+                    
+                    <ul>
+                        <li>sociale : nous garantissons la fiabilité des données sociales</li>
+                        <li>économique : nous apportons une contribution aux activités économiques.</li>
+                    </ul>
+
+                </p>
+    
+
+            </div>
+    
+            </section> 
+
+            <section id="Commentaires">
+                
+                <div class="Mon_com">
+                    <h3>Mon commentaire</h3>
+                    
+                    <textarea name="commentary" rows="4"  placeholder="Commentez..."></textarea>
+                
+                
+                
+                </div>
+                            
+                
+                    
+                       
+            
+            
+            
+            
+            
+            
+            </section>
+
+
+
+
+
+
+            <footer class="site-footer">
+                <p class="mentions"> Mentions légales | Contacts</p>
+
+
+
+        </div>
+    </body>
+</html>
